85b272c6552f1ca9eeb45b08db5f346e18721d56
damansetia <77336655+damansetia@users.noreply.github.com>
2021-05-06 10:50:43 +0530

    Add files via upload

---
+++ congurentRandomNumber.py
@@ -0,0 +1,16 @@
+from random import randrange
+randomNums=[0]*10	
+randomNums[0] = 5 
+
+
+	
+for i in range(1, 10): 
+		
+	randomNums[i] = ((randomNums[i - 1] * 3) +
+										3) % 7 
+print("Random Number using congurent")
+for i in randomNums: 
+	print(i, end = " ") 
+print("\nRandom number using built-in functions")
+for i in range(10):
+    print(randrange(10),end=" ")
